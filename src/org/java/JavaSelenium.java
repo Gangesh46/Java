b1dcb6fc7d18e2a1d5f74e08552b43fb86cde22b
Gangesh46 <gangesh46.sandy@gmail.com>
2021-01-07 12:45:46 +0530

    code commit

---
+++ src/org/java/JavaSelenium.java
@@ -0,0 +1,13 @@
+package org.java;
+
+public class JavaSelenium {
+
+	public void office() {
+		System.out.println("Infosys");
+	}	
+		public void branch() {
+			System.out.println("Chennai");
+		}
+	
+		
+}
